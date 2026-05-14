9126216441eca320c93ac656ff2390268d4e0ead
Ram Pandian <rampandian47@gmail.com>
2026-05-14 11:14:12 +0530

    Initial commit

---
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Map<String, String> hello() {
+
+        Map<String, String> response = new HashMap<>();
+
+        response.put("message", "DevOps Project Running Successfully");
+
+        return response;
+    }
+}
